d2b33d1ccfb948b726918783f312d29bc2434e25
Krishna <202389@AMB00671-2.local>
2017-07-21 09:16:50 +0530

    Correction

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,19 +2,19 @@
 
 set -e +x
 
-pushd movie-fun-service
+pushd movie-fun
   echo "Packaging WAR"
   ./mvn clean package -DskipTests -Dmaven.test.skip=true
 popd
 
-jar_count=`find apps-movie-fun-code/target -type f -name *.war | wc -l`
+jar_count=`find movie-fun/target -type f -name *.war | wc -l`
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one jar found, don't know which one to deploy. Exiting"
   exit 1
 fi
 
-find apps-movie-fun-code/target -type f -name *.war -exec cp "{}" package-output/moviefun.war \;
+find movie-fun/target -type f -name *.war -exec cp "{}" package-output/moviefun.war \;
 
 echo "Done packaging"
 exit 0
